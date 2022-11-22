410f5f02ede68453c0f5333e0ff99a2eac74c820
lscd <fshi3@u.rochester.edu>
2022-11-22 01:13:30 -0500

    separate page for rankedlist

--- front-qdvote/components/ShowEvents.js
+++ front-qdvote/components/ShowEvents.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { utils } from "web3";
 import styles from "./ShowEvents.module.css";
+import {logit} from "../pages/index.js";
 
 export default function ShowEvents() {
     const [Events, setEvents] = useState([]);
@@ -28,11 +29,11 @@ export default function ShowEvents() {
     useEffect(() => {
         setEvents(pushToArray());
     });
-   
+
     return (
         <div>
             {Events.map((each) => (
-              <div className={styles.events}>Event {each.id}:
+              <div className={styles.events} onClick={logit}>Event {each.id}:
               <br></br>
               <br></br>
               {each.title}</div>
