ec20a76571f159e26a4694e54a95dd534b6371c0
lscd <fshi3@u.rochester.edu>
2022-11-21 22:31:57 -0500

    gg

--- front-qdvote/pages/index.js
+++ front-qdvote/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
       <Header />
       <CreateItem />
-      <Rankedlist />
+      {/* <Rankedlist /> */}
       {/* End of Page */}
     </div>
   );
