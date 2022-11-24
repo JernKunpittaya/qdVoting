77384145050b840ea51de30282926ee5b2cafc33
lscd <fshi3@u.rochester.edu>
2022-11-24 16:51:08 -0500

    UI updated
    changed some files. home page to create new event. clicked on event to go to create item and rankedlist page

--- front-qdvote/components/ShowEvents.js
+++ front-qdvote/components/ShowEvents.js
@@ -4,33 +4,49 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { utils } from "web3";
 import styles from "./ShowEvents.module.css";
-import {logit} from "../pages/index.js";
 import { renderMatches } from "react-router";
+import { clickRanklist } from "../pages/index.js";
+import CreateItem from "../components/CreateItem";
+import Rankedlist from "../components/Rankedlist";
 
 export default function ShowEvents() {
     const [Events, setEvents] = useState([]);
     const [title, setTitle] = useState("");
     const [id, setId] = useState("");
     const { chainId: chainIdHex, web3, isWeb3Enabled, account } = useMoralis();
+    const [isClickedEvent, setIsClickedEvent] = useState(false);
+
+    function clickBack() {
+        setIsClickedEvent(false);
+    };
+
+    function clickRanklist() {
+        setIsClickedEvent(true);
+        console.log("click");
+    };
 
     // hard code some events with an event array to mock display
-    const event1 = {
-        title: "Favorite Basketball Player",
-        id: 1
+    // const event1 = {
+    //     title: "Favorite Basketball Player",
+    //     id: 1
+    // };
+    // const event2 = {
+    //     title: "Where to vacation?",
+    //     id: 2
+    // };
+    // const event3 = {
+    //     title: "Best fruits",
+    //     id: 3
+    // };
+    // function pushToArray() {
+    //     const arr = [];
+    //     arr.push(event1, event2, event3);
+    //     return arr;
+    // }
+
+    function logit() {
+        console.log(1);
     };
-    const event2 = {
-        title: "Where to vacation?",
-        id: 2
-    };
-    const event3 = {
-        title: "Best fruits",
-        id: 3
-    };
-    function pushToArray() {
-        const arr = [];
-        arr.push(event1, event2, event3);
-        return arr;
-    }
 
     useEffect(() => {
 
@@ -38,55 +54,58 @@ export default function ShowEvents() {
 
     function createEvent(e) {
         e.preventDefault();
-        setEvents([...Events, { title: title, id: id }]);
+        // ID is auto incrementing
+        setEvents([...Events, { title: title, id: Events.length+1 }]);
     };
 
     return (
         <div>
+        {!isClickedEvent && (
             <div>
-            {isWeb3Enabled && (
                 <div className={styles.section}>
-                <h2 className={styles.title}>Create an Event {"📝"}</h2>
-                <form onSubmit={createEvent} className={styles.forms}>
-                    <input
-                    type="text"
-                    value={title}
-                    placeholder="Title"
-                    required
-                    onChange={(e) => setTitle(e.target.value)}
-                    className = {styles.input}
-                    />
-                    <br />
-                    <input
-                    type="text"
-                    value={id}
-                    placeholder="ID"
-                    required
-                    onChange={(e) => setId(e.target.value)}
-                    className = {styles.input}
-                    />
-                    <br />
-                    <input type="submit" className= {styles.button}/>
-                </form>
-                {/* <button onClick={itemCount}>Count Item</button> */}
+                    <h2 className={styles.title}>Create an Event {"📝"}</h2>
+                    <form onSubmit={createEvent} className={styles.forms}>
+                        <input
+                        type="text"
+                        value={title}
+                        placeholder="Title"
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                        className = {styles.input}
+                        />
+                        <br />
+                        {/* <input
+                        type="text"
+                        value={id}
+                        placeholder="ID"
+                        required
+                        onChange={(e) => setId(e.target.value)}
+                        className = {styles.input}
+                        />
+                        <br /> */}
+                        <input type="submit" className= {styles.button}/>
+                    </form>
+                    {/* <button onClick={itemCount}>Count Item</button> */}
                 </div>
-            )}
-            {!isWeb3Enabled && (
                 <div>
-                <h1>Welcome!</h1>
-                <h2>connect ur wallet first!</h2>{" "}
+                    {Events.map((each) => (
+                    <div className={styles.events} onClick={ clickRanklist }>Event {each.id}:
+                    <br></br>
+                    <br></br>
+                    {each.title}</div>
+                    ))}
                 </div>
-            )}
             </div>
+        )}
+        {isClickedEvent && (
             <div>
-                {Events.map((each) => (
-                <div className={styles.events} onClick={logit}>Event {each.id}:
-                <br></br>
-                <br></br>
-                {each.title}</div>
-                ))}
+                <button onClick={clickBack}>Back</button>
+                <CreateItem />
+                <Rankedlist />
             </div>
+        )}
         </div>
+
       );
 
 }
