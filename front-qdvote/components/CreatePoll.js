ad5e840110e3d9f5517a9d12eb6e3c4cab04adce
lscd <fshi3@u.rochester.edu>
2022-12-19 15:11:47 -0500

    front end change to seconds

--- front-qdvote/components/CreatePoll.js
+++ front-qdvote/components/CreatePoll.js
@@ -12,7 +12,7 @@ export default function CreatePoll() {
   const [eligibleFields, setEligibleFields] = useState([1]); // number of textfields for eligible voters
   const [eligibles, setEligibles] = useState([""]); // addresses that are eligible to vote
   const [options, setOptions] = useState([{ name: "", description: "" }]); // options of the current Event
-  const [validMins, setValidMins] = useState(30); // default 30 minutes
+  const [validSecs, setValidSecs] = useState(1200); // default 1200 seconds
   const { chainId: chainIdHex, web3, isWeb3Enabled, account } = useMoralis();
   const chainId = parseInt(chainIdHex);
   const quadraticVotingAddress =
@@ -102,8 +102,8 @@ export default function CreatePoll() {
     console.log(title);
     console.log(optionNames);
     console.log(eligibles);
-    console.log(validMins);
-    await contract.createPoll(title, eligibles, optionNames, validMins);
+    console.log(validSecs);
+    await contract.createPoll(title, eligibles, optionNames, validSecs);
     alert("Event Successfully Created");
   }
 
@@ -195,13 +195,13 @@ export default function CreatePoll() {
         </button>
         <br />
         <h3 className={styles.title}>
-          & enter how long poll will last (in minutes):
+          & enter how long poll will last (in seconds):
         </h3>
         <input
           type="text"
-          placeholder="30"
+          placeholder="1200"
           required
-          onChange={(e) => setValidMins(e.target.value)}
+          onChange={(e) => setValidSecs(e.target.value)}
           className={styles.input}
         />
         <br />
